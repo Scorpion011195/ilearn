9757a525df74dc99baf287b74f021e9160d4bb27
SCORPION <nguyenthanhat011195@gmail.com>
2017-06-26 16:27:04 +0700

    Update

--- storage/framework/views/b6c00b742487c43beb04edaa36aab74db77fe68c.php
+++ storage/framework/views/b6c00b742487c43beb04edaa36aab74db77fe68c.php
@@ -1,14 +1,3 @@
-<?php $__env->startSection('content-header'); ?>
-        <h1>
-            Thêm file scv
-            <small><?php echo e(Session::get('user')->username); ?></small>
-        </h1>
-        <!-- <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-            <li class="active">Here</li>
-        </ol> -->
-<?php $__env->stopSection(); ?>
-
 <?php $__env->startSection('css'); ?>
     <link href="https://cdn.datatables.net/1.10.15/css/jquery.dataTables.min.css"
           rel="stylesheet"/>
@@ -17,37 +6,21 @@
           rel="stylesheet"/>
 <?php $__env->stopSection(); ?>
 
-<?php $__env->startSection('script'); ?>
-    <!-- X-EDITABLE JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.0/bootstrap3-editable/js/bootstrap-editable.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            $.fn.editable.defaults.mode = 'inline';
-            $(document).ready(function() {
-                $(' td ').editable();
-            });
-            $('#username').editable({
-                type: 'text',
-                pk: 1,
-                url: '/post',
-                title: 'Enter username'
-            });
-            var table = $('#example').DataTable();
-
-            $('#example tbody').on('click', 'tr', function () {
-                var data = table.row( this ).data();
-                alert( 'You clicked on '+data[0]+'\'s row' );
-            } );
-
-        } );
-    </script>
+<?php $__env->startSection('content-header'); ?>
+        <h1>
+            Thêm file scv
+            <!-- <small><?php echo e(utf8_encode(Session::get('user')->username)); ?></small> -->
+        </h1>
+        <!-- <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
+            <li class="active">Here</li>
+        </ol> -->
 <?php $__env->stopSection(); ?>
 
 <?php $__env->startSection('content'); ?>
     <div class="panel">
         <div class="panel-body">
-            <?php echo Form::open(); ?>
+            <?php echo Form::open(array('enctype' => 'multipart/form-data', 'files' =>true, 'accept-charset' => 'utf-8')); ?>
 
             <div class="panel-content">
                 <?php echo Form::label('csv-file', 'Tải lên file csv'); ?>
@@ -64,11 +37,17 @@
 
     </div>
 
-    <div class="panel">
-        <div class="panel-body">
-            <?php echo $__env->make('backend.layout.partial.dict-table', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
-        </div>
-    </div>
+    <?php if(isset($info)): ?>
+        <div class='col-md-3'></div>
+            <div class="col-md-6">
+                <div class="alert alert-info" id='idsuccess'>
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>               
+                <h2 align="center">Upload file successfully</h2>
+                </div>
+            </div>
+    <?php endif; ?>
+    
 <?php $__env->stopSection(); ?>
+
 
 <?php echo $__env->make('backend.ilearn', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
