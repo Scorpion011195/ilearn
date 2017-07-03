a3518457a007bd11f69160fbe4dd5cbec0e192cc
SCORPION <nguyenthanhat011195@gmail.com>
2017-07-03 14:12:14 +0700

    no message

--- resources/views/frontend/layout/result.blade.php
+++ resources/views/frontend/layout/result.blade.php
@@ -5,26 +5,6 @@
         <div class = "col-sm-6 col-sm-offset-3 form-group row">
             <div class ="row panel panel-default no-radius-left no-radius-right">
                 <div class="col-xs-8 col-sm-6">
-                    <span class="title">Bản dịch từ:{!! ucfirst($inputText) !!}</span>
-                    <form action="{{route('HistoryAddNew')}}" method="POST" role="form">
-                        @if(isset($workInfo))
-                        <?php $type = '' ?>
-                        @foreach ($workInfo as $language)
-                        <?php $getData ='';?>
-                        @if(!($type == $language->type))
-                        <?php  $type = $language->type ?>
-                        <b class="_type" id="type">{!! $language->type !!}</b>:<br>
-                        @if(!Auth::guest())
-                        <span class="glyphicon glyphicon-plus _push-his" id="_id{!! $language->id !!}" data="{!! $language->id !!}">{!! $language->listen !!}</span>
-                        <span class="glyphicon glyphicon-volume-up" id="_id{!! $language->id !!}">{!! $language->listen !!}</span>
-                        <span contenteditable> {!! $language ->word !!} </span> &nbsp;<br>
-                        @endif
-                        @if(Auth::guest())
-                        <span class="glyphicon glyphicon-plus">{!! $language->listen !!}</span>
-                        <span class="glyphicon glyphicon-volume-up">{!! $language->listen !!}</span>
-                        <span> {!! $language ->word !!} </span> &nbsp;<br>
-                        @endif
-                        @else
                     <span class="title">Bản dịch từ:{!! ($inputText) !!}</span>
                     <form action="{{route('HistoryAddNew')}}" method="POST" role="form">
                         @if(isset($workInfo))
@@ -119,57 +99,101 @@
 <!-- Modal for edit word : Editer: Trong 10/40/AM/2017/26/06-->
 <div id="myModal" class="modal fade" role="dialog">
   <div class="modal-dialog">
->>>>>>> scr_guest_translate
 
-                        @if(!Auth::guest())
-                        <span class="glyphicon glyphicon-plus _push-his" id="_id{!! $language->id !!}" data-id="{!! $language->id !!}">{!! $language->listen !!}</span>
-                        <span class="glyphicon glyphicon-volume-up" id="_id{!! $language->id !!}">{!! $language->listen !!}</span>
-                        <span contenteditable> {!! $language ->word !!} </span> &nbsp;<br>
-                        @endif
-                        @if(Auth::guest())
-                        <span class="glyphicon glyphicon-plus">{!! $language->listen !!}</span>
-                        <span class="glyphicon glyphicon-volume-up">{!! $language->listen !!}</span>
-                        <span> {!! $language ->word !!} </span> &nbsp;<br>
-                        @endif
-                        @if(Auth::guest())
-                        <div class="right-group">
-                            <a href="javascript:void(0);" onclick="loginToAdd()">Đăng nhập</a> để có thêm nhiều tiện ích
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Chỉnh sửa từ</h4>
+    </div>
+    <div class="modal-body">
+        <form action="{{ route('historyUpdate') }}" method="POST" role="form" id="create-dict-form">
+           <input type="hidden" name="_token" value="{{ csrf_token() }}">
+           <div class="create-dict-input">
+            <div class="row panel">
+                <div class="row no-margin margin-top">
+                    <div class="col-sm-5">
+                        <div class="row">
+                            <div class="col-sm-3">
+                                <label  class="form-label text-center-vertical">Từ</label>
+                            </div>
+                            <div class="col-sm-9">
+                                <input type="text" class="form-control" value="{{$inputText}}" name="tu">
+                            </div>
                         </div>
-                        @endif
+                    </div>
 
-                        @if(!Auth::guest())
-                        <div class="right-group">
-                            <h5 style="float: right;"><b style="color: red;"><i class="fa fa-sticky-note" aria-hidden="true"></i> NOTE:</b>Nhấn vào từ để chỉnh sửa</h5>
+
+                    <div class="col-sm-6">
+                        <div class="row">
+                            <div class="col-sm-5">
+                                <label  class="form-label text-center-vertical">Ngôn ngữ</label>
+                            </div>
+                            <div class="col-sm-7">
+                                <input type="text" class="form-control" name="cb1" value="{{$langueInput}}">
+                            </div>
                         </div>
-                        @endif
-                    </form>
-                    @endif
-                    @endforeach
-                    @endif
+                    </div>
                 </div>
-                <div class="col-xs-4 col-sm-6">
-                    <span class="title">Giải thích từ:{!! ucfirst($inputText) !!}</span>
-                    @if(isset($workInfo))
-                    <?php $type = '' ?>
-                    @foreach ($workInfo as $language)
-                    <?php
-                    $getData='';
-                    ?>
-                    @if(is_null($language->explain))
-                    @else
-                    @if(!($type == $language->type))
-                    <?php  $type = $language->type ?><br>
-                    <b class="_type" >{!! $language->type !!}</b>:<br>
-                    <span> {!! $language ->explain !!} </span> &nbsp;<br>
-                    @else
-                    <span> {!! $language ->explain !!} </span> &nbsp;<br>
-                    @endif
-                    @endif
-                    @endforeach
-                    @endif
+                <div class="row no-margin margin-top">
+                    <div class="col-sm-5">
+                        <div class="row">
+                            <div class="col-sm-3">
+                               <label  class="form-label text-center-vertical">Giải thích</label>
+                           </div>
+                           <div class="col-sm-9">
+                            <input type="text" class="form-control" name="des1">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="row no-margin margin-top">
+            <div class="col-sm-5">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <label  class="form-label text-center-vertical">Nghĩa</label>
+                    </div>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" name="nghia" value="{{ $language->word}}">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6">
+                <div class="row">
+                    <div class="col-sm-5">
+                        <label  class="form-label text-center-vertical">Ngôn ngữ</label>
+                    </div>
+                    <div class="col-sm-7">
+                        <input type="text" class="form-control" name="cb2" value="{{$langueOutput}}">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row no-margin margin-top">
+            <div class="col-sm-5">
+                <div class="row">
+                    <div class="col-sm-3">
+                       <label  class="form-label text-center-vertical">Giải thích</label>
+                   </div>
+                   <div class="col-sm-9">
+                    <input type="text" class="form-control" name="des2">
                 </div>
             </div>
         </div>
     </div>
 </div>
+<div class="text-center row margin-top">
+    {!! Form::submit("Send", ['class' => 'btn btn-success']) !!}
+</div>
+{!! Form::close() !!}
+</div>
+<div class="modal-footer">
+    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+</div>
+</div>
 
+</div>
+</div>
