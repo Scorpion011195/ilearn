8a98e2df705aa6fee2e690a29c558a3cf61e4172
SCORPION <nguyenthanhat011195@gmail.com>
2017-07-05 09:49:56 +0700

    Update Push Notification 20170705

--- database/seeds/HistorysTableSeeder.php
+++ database/seeds/HistorysTableSeeder.php
@@ -12,27 +12,27 @@ class HistorysTableSeeder extends Seeder
     public function run()
     {
         DB::table('historys')->insert([
-            ['id_history'=>1,'content'=>'[{"type_to":"Thán từ","STT":1,"from":"english","to":"vietnamese","from_text":"hello","to_text":"xin ch\u00e0o","notification":"F"},
- {"type_to":"Thán từ","STT":2,"from":"japanese","to":"vietnamese","from_text":"こんにちは","to_text":"xin ch\u00e0o","notification":"F"},
-{"type_to":"間投詞","STT":3,"from":"english","to":"japanese","from_text":"hello","to_text":"こんにちは","notification":"F"}]'],
-            ['id_history'=>2,'content'=>'[{"type_to":"Thán từ","STT":1,"from":"english","to":"vietnamese","from_text":"hello","to_text":"xin ch\u00e0o","notification":"F"},
- {"type_to":"Thán từ","STT":2,"from":"japanese","to":"vietnamese","from_text":"こんにちは","to_text":"xin ch\u00e0o","notification":"F"},
-{"type_to":"間投詞","STT":3,"from":"english","to":"japanese","from_text":"hello","to_text":"こんにちは","notification":"F"}]'],
-            ['id_history'=>3,'content'=>'[{"type_to":"Thán từ","STT":1,"from":"english","to":"vietnamese","from_text":"hello","to_text":"xin ch\u00e0o","notification":"F"},
- {"type_to":"Thán từ","STT":2,"from":"japanese","to":"vietnamese","from_text":"こんにちは","to_text":"xin ch\u00e0o","notification":"F"},
-{"type_to":"間投詞","STT":3,"from":"english","to":"japanese","from_text":"hello","to_text":"こんにちは","notification":"F"}]'],
-            ['id_history'=>4,'content'=>'[{"type_to":"Thán từ","STT":1,"from":"english","to":"vietnamese","from_text":"hello","to_text":"xin ch\u00e0o","notification":"F"},
- {"type_to":"Thán từ","STT":2,"from":"japanese","to":"vietnamese","from_text":"こんにちは","to_text":"xin ch\u00e0o","notification":"F"},
-{"type_to":"間投詞","STT":3,"from":"english","to":"japanese","from_text":"hello","to_text":"こんにちは","notification":"F"}]'],
-            ['id_history'=>5,'content'=>'[{"type_to":"Thán từ","STT":1,"from":"english","to":"vietnamese","from_text":"hello","to_text":"xin ch\u00e0o","notification":"F"},
- {"type_to":"Thán từ","STT":2,"from":"japanese","to":"vietnamese","from_text":"こんにちは","to_text":"xin ch\u00e0o","notification":"F"},
-{"type_to":"間投詞","STT":3,"from":"english","to":"japanese","from_text":"hello","to_text":"こんにちは","notification":"F"}]'],
-            ['id_history'=>6,'content'=>'[{"type_to":"Thán từ","STT":1,"from":"english","to":"vietnamese","from_text":"hello","to_text":"xin ch\u00e0o","notification":"F"},
- {"type_to":"Thán từ","STT":2,"from":"japanese","to":"vietnamese","from_text":"こんにちは","to_text":"xin ch\u00e0o","notification":"F"},
-{"type_to":"間投詞","STT":3,"from":"english","to":"japanese","from_text":"hello","to_text":"こんにちは","notification":"F"}]'],
-            ['id_history'=>7,'content'=>'[{"type_to":"Thán từ","STT":1,"from":"english","to":"vietnamese","from_text":"hello","to_text":"xin ch\u00e0o","notification":"F"},
- {"type_to":"Thán từ","STT":2,"from":"japanese","to":"vietnamese","from_text":"こんにちは","to_text":"xin ch\u00e0o","notification":"F"},
-{"type_to":"間投詞","STT":3,"from":"english","to":"japanese","from_text":"hello","to_text":"こんにちは","notification":"F"}]']
+            ['id_history'=>1,'content'=>'[{"type_to":"Thán từ","from":"english","to":"vietnamese","from_text":"hello","to_text":"xin ch\u00e0o","notification":"F"},
+ {"type_to":"Thán từ","from":"japanese","to":"vietnamese","from_text":"こんにちは","to_text":"xin ch\u00e0o","notification":"F"},
+{"type_to":"間投詞","from":"english","to":"japanese","from_text":"hello","to_text":"こんにちは","notification":"F"}]'],
+            ['id_history'=>2,'content'=>'[{"type_to":"Thán từ","from":"english","to":"vietnamese","from_text":"hello","to_text":"xin ch\u00e0o","notification":"F"},
+ {"type_to":"Thán từ","from":"japanese","to":"vietnamese","from_text":"こんにちは","to_text":"xin ch\u00e0o","notification":"F"},
+{"type_to":"間投詞","from":"english","to":"japanese","from_text":"hello","to_text":"こんにちは","notification":"F"}]'],
+            ['id_history'=>3,'content'=>'[{"type_to":"Thán từ","from":"english","to":"vietnamese","from_text":"hello","to_text":"xin ch\u00e0o","notification":"F"},
+ {"type_to":"Thán từ","from":"japanese","to":"vietnamese","from_text":"こんにちは","to_text":"xin ch\u00e0o","notification":"F"},
+{"type_to":"間投詞","from":"english","to":"japanese","from_text":"hello","to_text":"こんにちは","notification":"F"}]'],
+            ['id_history'=>4,'content'=>'[{"type_to":"Thán từ","from":"english","to":"vietnamese","from_text":"hello","to_text":"xin ch\u00e0o","notification":"F"},
+ {"type_to":"Thán từ","from":"japanese","to":"vietnamese","from_text":"こんにちは","to_text":"xin ch\u00e0o","notification":"F"},
+{"type_to":"間投詞","from":"english","to":"japanese","from_text":"hello","to_text":"こんにちは","notification":"F"}]'],
+            ['id_history'=>5,'content'=>'[{"type_to":"Thán từ","from":"english","to":"vietnamese","from_text":"hello","to_text":"xin ch\u00e0o","notification":"F"},
+ {"type_to":"Thán từ","from":"japanese","to":"vietnamese","from_text":"こんにちは","to_text":"xin ch\u00e0o","notification":"F"},
+{"type_to":"間投詞","from":"english","to":"japanese","from_text":"hello","to_text":"こんにちは","notification":"F"}]'],
+            ['id_history'=>6,'content'=>'[{"type_to":"Thán từ","from":"english","to":"vietnamese","from_text":"hello","to_text":"xin ch\u00e0o","notification":"F"},
+ {"type_to":"Thán từ","from":"japanese","to":"vietnamese","from_text":"こんにちは","to_text":"xin ch\u00e0o","notification":"F"},
+{"type_to":"間投詞","from":"english","to":"japanese","from_text":"hello","to_text":"こんにちは","notification":"F"}]'],
+            ['id_history'=>7,'content'=>'[{"type_to":"Thán từ","from":"english","to":"vietnamese","from_text":"hello","to_text":"xin ch\u00e0o","notification":"F"},
+ {"type_to":"Thán từ","from":"japanese","to":"vietnamese","from_text":"こんにちは","to_text":"xin ch\u00e0o","notification":"F"},
+{"type_to":"間投詞","from":"english","to":"japanese","from_text":"hello","to_text":"こんにちは","notification":"F"}]']
             ]);
     }
 }
