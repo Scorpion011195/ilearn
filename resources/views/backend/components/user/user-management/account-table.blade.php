232691a8a48f03058c940572095da64cfcd5ea59
SCORPION <nguyenthanhat011195@gmail.com>
2017-07-07 23:50:39 +0700

    no message

--- resources/views/backend/components/user/user-management/account-table.blade.php
+++ resources/views/backend/components/user/user-management/account-table.blade.php
@@ -10,7 +10,6 @@
   @endif
 @endif
     <!-- /.box-header -->
-    <div class="box-body">
         <div id="example_wrapper" class="dataTables_wrapper form-inline dt-bootstrap">
             <div class="row">
                 <div class="col-sm-12 table-responsive">
@@ -108,6 +107,7 @@
                     </table>
                 </div>
             </div>
+        </div>
         <div class="row">
             <div class="col-sm-5">
                 <!-- <div class="dataTables_info" id="example1_info" role="status" aria-live="polite">Hiển thị {{ $noOfPages }} của {{ $noOfAccounts }} tài khoản -->
@@ -116,6 +116,5 @@
             </div>
             <div class="col-sm-7"></div>
         </div>
-    </div>
 {!! $accounts->links() !!}
 
