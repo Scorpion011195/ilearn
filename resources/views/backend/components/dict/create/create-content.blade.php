e2cad9360580d1d26ea67c4e1333db5e388f99f9
SCORPION <nguyenthanhat011195@gmail.com>
2017-07-10 08:56:50 +0700

    no message

--- resources/views/backend/components/dict/create/create-content.blade.php
+++ resources/views/backend/components/dict/create/create-content.blade.php
@@ -63,7 +63,10 @@
           <div class="col-sm-6 {{ $errors->has('_txttu') ? ' has-error' : '' }}">
             <div class="input-group ">
               <span class="input-group-addon">Từ</span>
-              <input size="30" id="msg" type="text" class="form-control" name="_txttu" placeholder="hello" value="{!! old('_txttu') !!}" required maxlength="50">
+              <input size="30" id="msg" required maxlength="50" type="text" class="form-control" name="_txttu" placeholder="hello"
+              @if (!$errors->has('_txttu'))
+                value="{!! old('_txttu') !!}"
+              @endif >
             </div>
             @if ($errors->has('_txttu'))
             <span class="glyphicon glyphicon-warning-sign help-block--color-apple-blossom"></span>   <strong class="help-block--color-apple-blossom">{!! $errors->first('_txttu') !!}</strong>
@@ -72,7 +75,10 @@
           <div class="col-sm-6 {{ ($errors->has('_txtnghia')&&!($errors->has('_txttu'))) ? ' has-error' : '' }}">
             <div class="input-group">
               <span class="input-group-addon">Nghĩa</span>
-              <input size="30" id="msg" type="text" class="form-control" name="_txtnghia" placeholder="xin chào" value="{!! old('_txtnghia') !!}" required maxlength="50">
+              <input size="30" id="msg" required maxlength="50" type="text" class="form-control" name="_txtnghia" placeholder="xin chào"
+              @if (!$errors->has('_txtnghia'))
+                value="{!! old('_txtnghia') !!}"
+              @endif >
             </div>
             @if ($errors->has('_txtnghia')&&!($errors->has('_txttu')))
             <span class="glyphicon glyphicon-warning-sign help-block--color-apple-blossom"></span>   <strong class="help-block--color-apple-blossom">{!! $errors->first('_txtnghia') !!}</strong>
